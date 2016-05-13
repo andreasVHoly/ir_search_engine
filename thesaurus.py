67bff0301cd4af5b5a861159ff66fdc1ed2d6bb9
Alon Bresler <brsalo001@herring.cs.uct.ac.za>
2016-05-13 21:15:00 +0200

    Thesaurus.py now returns a list of unique synonyms for the entered term.

--- thesaurus.py
+++ thesaurus.py
@@ -4,5 +4,11 @@ from nltk.corpus import wordnet as wn
 
 class Thesaurus:
 
+    #returns a list of synonyms for the entered word
     def getSynonym(self, word):
-        return wn.synsets(word)
+        synonyms = []
+        for synset in wn.synsets(word):
+            for lemma in synset.lemmas():
+                if lemma.name() not in synonyms:
+                    synonyms.append(lemma.name())
+        return synonyms
