f3b26c2e470d858e9336828b5f4d1e34699bf361
Alon Bresler <brsalo001@herring.cs.uct.ac.za>
2016-05-14 13:01:34 +0200

    MAP complete - calculate map for 5 queries of a collection.

--- MAP.py
+++ MAP.py
@@ -1,10 +1,47 @@
-
+import re
 import os
 import sys
 import subprocess
 
-def getDocID(strResult):
-    print("x")
+def calcAllMAP():
+    totMap = 0
+    for i in range(1,6):
+        docIDs = getResults("./Results/results." + str(i))
+        totMap+=calcMAP(i, docIDs)
+    return totMap/5
+
+def calcMAP(q, dIDs):
+    map = 0
+    pos = 1
+    for d in dIDs:
+        map += getRel(q, d, pos)
+        pos+=1
+    map /= len(dIDs)
+    return map
+
+def getRel(q, ID, pos):
+    fileName = "./" + sys.argv[1] + "/relevance." + str(q)
+    fo = open(fileName, "r")
+    rel = fo.readlines()
+
+    if int(rel[int(ID)]) == 0:
+        return int(rel[int(ID)])/pos
+    elif int(rel[int(ID)]) == 1:
+        return (int(rel[int(ID)])/pos)*1.5
+    elif int(rel[int(ID)]) == 2:
+        return 1
+
+def getResults(resultFile):
+    dID = []
+    fo = open(resultFile, "r")
+    lines = fo.readlines()
+    for line in lines:
+        line = line.rstrip('\n')
+        result = line.split(" ")
+        if len(result) > 2:
+            if result[1] != "results":
+                dID.append(result[1])
+    return dID
 
 #Run the queries in the collection to create files with the results
 def runQueries(collection):
@@ -18,11 +55,16 @@ def runQueries(collection):
         with open(fileName, "w+") as output:
             subprocess.call(["python3", "query.py", sys.argv[1], query], stdout=output);
 
+    sys.stdout.write('\r'+ "All results collected." + query + (" " * 30))
+    print("")
+    sys.stdout.write('\r'+ "Calculating MAP..." + (" " * 30))
+
 if len(sys.argv)<2:
    print ("Syntax: MAP.py <collection>")
    exit(0)
 
 runQueries(sys.argv[1])
+sys.stdout.write('\r'+ "MAP = " + str(calcAllMAP()) + (" " * 10))
 print("")
 
 
