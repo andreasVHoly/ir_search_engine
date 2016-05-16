e7a3292ec2ad1ebb12b51ec7812a450d36c109b0
Andreas <andreas409@gmail.com>
2016-05-16 07:40:42 +0200

    pushing

--- booleanSearch.py
+++ booleanSearch.py
@@ -1,4 +1,14 @@
 
 
-
+#
 def constructList(query):
+    splitquery = query.split("and")
+    newquery = []
+    print(splitquery)
+    for i in splitquery:
+        i = i.strip()
+        newquery.append(i)
+    print(newquery)
+
+
+constructList("this is a and test and what the hell and p")
